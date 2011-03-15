64593e316a00b46a11759c957c4336a9c888bd4a
Luiz Ribeiro <luizfar@gmail.com>
2011-03-15 01:15:39 -0300

    Added test to code written in 'plateau' function to handle the addition of ships.
    As JavaScript is not typed, we can just stub out the ships using empty objects!

--- spec/PlateauSpec.js
+++ spec/PlateauSpec.js
@@ -4,4 +4,17 @@ describe("plateau", function() {
 
     expect(p.element.css("position")).toEqual("relative");
   });
+
+  it("should contain ships", function() {
+    var p = plateau();
+
+    var s1 = {ship: 1};
+    var s2 = {ship: 2};
+
+    p.addShip(s1);
+    p.addShip(s2);
+
+    expect(p.ships()).toContain(s1);
+    expect(p.ships()).toContain(s2);
+  });
 });
