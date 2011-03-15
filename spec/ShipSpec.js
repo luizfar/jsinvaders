507159dae7b46bf0e9d0724b1d355726a7fb030f
Luiz Ribeiro <luizfar@gmail.com>
2011-03-15 01:22:56 -0300

    Replaced the creation of plateau objects for mocks in ShipSpec.js.
    Using Jasmine's spies we can easily ensure that a method has been called!

--- spec/ShipSpec.js
+++ spec/ShipSpec.js
@@ -1,15 +1,22 @@
 describe("ship", function() {
   it("should have an element with absolute position", function() {
-    var s = ship(plateau(point(0, 0), 100));
+    var s = ship(fakePlateau());
 
     expect(s.element.css("position")).toEqual("absolute");
   });
 
   it("should be added to a plateau", function() {
-    var p = plateau(point(0, 0), 100);
+    var p = fakePlateau();
+    spyOn(p, 'addShip');
 
     var s = ship(p);
 
-    expect(p.ships()).toContain(s);
+    expect(p.addShip).toHaveBeenCalledWith(s);
   });
+
+  function fakePlateau() {
+    return {
+      addShip: function() {}
+    };
+  }
 });
