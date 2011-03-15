e311175314f9fa096c15f12ea61366162ed3b53b
Luiz Ribeiro <luizfar@gmail.com>
2011-03-15 19:48:53 -0300

    Added moveLeft method to ship.
    Using the private method updatePosition to udpate the CSS position of the ship element on the screen!

--- js/Ship.js
+++ js/Ship.js
@@ -1,16 +1,28 @@
 function ship(plateau) {
   var self = square(plateau.initialShipPosition(), 15);
-
-  self.element = $("<div />");
-  self.element
+  self.position = function(newPosition) {
+    return self.corner = newPosition || self.corner;
+  }
+  
+  self.element = $("<div>")
     .css("position", "absolute")
     .css("width", self.side)
     .css("height", self.side)
-    .css("background", "red")
-    .css("top", self.corner.y)
-    .css("left", self.corner.x);
+    .css("background", "red");
 
   plateau.add(self);
+
+  function updatePosition() {
+    self.element
+      .css("top", self.corner.y)
+      .css("left", self.corner.x);
+  };
+  updatePosition();
+
+  self.moveLeft = function() {
+    self.position(self.position().shift({x: -10, y: 0}));
+    updatePosition();
+  };
 
   return self;
 }
